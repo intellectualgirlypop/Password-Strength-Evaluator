8bd0069d84c7dcb0fcc13c7c91ee34b25f9d508c
intellectualgirlypop <intellectualgirlypop@gmail.com>
2025-07-09 16:05:42 -0400

    checking input against attributes

--- password_project/src/main.rs
+++ password_project/src/main.rs
@@ -5,7 +5,16 @@ fn main() {
     println!("Please enter password for testing:"); 
 
     io::stdin().read_line(&mut user_input)
-        .expect("Failed to read password"); 
-        
+        .expect("Failed to read password");
     println!("The Password you entered is: '{}', ...Checking password strength...", user_input.trim());
+    let password = user_input.trim();
+    let bad_password = r"password_project\attributes.txt";{
+        if bad_password.contains(password){
+        println!("❌ This password is too generic. Score = 0. 
+        Please try again")
+        }else{
+        println!("✅ Password accepted, evaluating further ...")
+    }
+    }
 }
+
